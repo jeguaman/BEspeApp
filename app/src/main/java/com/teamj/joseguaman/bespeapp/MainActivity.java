a0aa75347ead5f32407cac98484ffff435af00a6
jose.guaman <jose.guaman@alterbios.com>
2017-07-04 14:44:24 -0500

    se cambia la navegacion al primero en la seleccion

--- app/src/main/java/com/teamj/joseguaman/bespeapp/MainActivity.java
+++ app/src/main/java/com/teamj/joseguaman/bespeapp/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_principal);
+        onNavigationItemSelected(navigationView.getMenu().getItem(0));
     }
 
     @Override
