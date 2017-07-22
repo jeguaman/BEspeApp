d625d0d246eef997a1be7dd0bf83f6a9e979ff5f
Jose Guaman <jeguaman1@gmail.com>
2017-07-21 19:18:44 -0500

    notificaciones rest client

--- app/src/main/java/com/teamj/joseguaman/bespeapp/webService/NotificacionRestClient.java
+++ app/src/main/java/com/teamj/joseguaman/bespeapp/webService/NotificacionRestClient.java
@@ -1,8 +1,35 @@
 package com.teamj.joseguaman.bespeapp.webService;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.teamj.joseguaman.bespeapp.modelo.beacon.WSResponse;
+import com.teamj.joseguaman.bespeapp.utils.Constants;
+import com.teamj.joseguaman.bespeapp.webService.restClientBase.GsonRequest;
+import com.teamj.joseguaman.bespeapp.webService.restClientBase.RestClientBase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Jose Guaman on 04/07/2017.
  */
 
-public class NotificacionRestClient {
+public class NotificacionRestClient extends RestClientBase {
+
+    public NotificacionRestClient(Context mContext) {
+        super(mContext);
+    }
+
+    public void getLugaresPorArea(String idArea, Response.Listener<WSResponse> listener,
+                                  Response.ErrorListener errorListener) {
+        Map<String, String> params = new HashMap<>();
+        Map<String, String> header = new HashMap<>();
+        params.put("id_area", idArea);
+
+        GsonRequest<WSResponse> request = new GsonRequest<>(Request.Method.POST, Constants.getURLRegistroAreaDispositivo(), WSResponse.class, header, params, listener, errorListener);
+        executeRequest(request);
+
+    }
 }
