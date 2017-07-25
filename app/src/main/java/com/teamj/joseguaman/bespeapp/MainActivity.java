cd397097baf529bcf8761f9edecd9acc789ba5fa
Jose Guaman <jeguaman1@gmail.com>
2017-07-24 23:49:42 -0500

    se hace merge en el main activity

--- app/src/main/java/com/teamj/joseguaman/bespeapp/MainActivity.java
+++ app/src/main/java/com/teamj/joseguaman/bespeapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
